0c527cc8be5fcc6cf049bb280966386946026e59
Elias Lima <eliaslima900@hotmail.com>
2021-02-04 19:54:50 -0300

    Refactor JSP pages and add a new attribute to Company model

--- WebContent/confirmRegister.jsp
+++ WebContent/confirmRegister.jsp
@@ -1,13 +1,14 @@
-
-<%
-String companyName = (String) request.getAttribute("companyName");
-System.out.println(companyName);
-%>
-
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 
 <!DOCTYPE html>
 <html>
 <body>
-	<h1> <%= companyName %>  cadastrado(a) com sucesso! </h1>
+	<c:if test="${ not empty companyName }">
+		<h1>${ companyName } Cadastrada com sucesso!</h1>
+	</c:if>
+	
+	<c:if test="${ empty companyName }">
+		<h1> Nenhuma empresa foi cadastrada </h1>
+	</c:if>
 </body>
 </html>
--- src/br/com/zup/gerenciador/servlet/ListCompaniesServlet.java
+++ src/br/com/zup/gerenciador/servlet/ListCompaniesServlet.java
@@ -3,14 +3,16 @@ package br.com.zup.gerenciador.servlet;
 import java.io.IOException;
 import java.util.List;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import br.com.zup.gerenciador.models.Company;
 import br.com.zup.gerenciador.models.DataBase;
-import jakarta.servlet.RequestDispatcher;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+
 
 @WebServlet("/listCompanies")
 public class ListCompaniesServlet extends HttpServlet {
@@ -22,11 +24,10 @@ public class ListCompaniesServlet extends HttpServlet {
 			throws ServletException, IOException {
 
 		List<Company> companies = DB.getCompanies();
+		request.setAttribute("companies", companies);
 
 		RequestDispatcher rd = request.getRequestDispatcher("/listCompanies.jsp");
-		request.setAttribute("companies", companies);
 		rd.forward(request, response);
-
 	}
 
 }
